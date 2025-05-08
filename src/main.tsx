e6500b40e130e5703f7cc9375c21e13d6e0c94e2
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2025-05-08 21:17:11 +0200

    change import for theme file

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { createTheme, ThemeProvider } from "@mui/material";
-import { themeOptions } from "./assets/theme/light.ts";
+import { themeOptions } from "./assets/theme/theme.ts";
 
 const theme = createTheme(themeOptions);
 
