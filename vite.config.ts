51e91fffe062c4ee6efd06b7d8bcc148f668b071
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2025-06-07 15:41:58 +0200

    change vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://sklinkusch.github.io/konklave2025/",
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://konklave2025.vercel.app",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
