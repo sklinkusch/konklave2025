b4952c6f938cd3eec676504a45f183ef5e9028bd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2025-08-03 16:47:12 +0200

    add full routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import Konklave from "@pages/Konklave";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { getCardinals } from "@assets/data";
+import { Home } from "@pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/2025"
-          element={<Konklave year={2025} cardinals={getCardinals("2025")} />}
-        />
-        <Route
-          path="/2013"
-          element={<Konklave year={2013} cardinals={getCardinals("2013")} />}
-        />
+        <Route path="/" element={<Home />} />
+        <Route path="/year/:year" element={<Konklave />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/FirstName/FirstName.tsx
+++ src/components/FirstName/FirstName.tsx
@@ -1,13 +1,13 @@
 import { Typography } from "@mui/material";
 import { Table } from "../Table/Table";
 
-const FirstName = ({ name, data }: FirstNameProps) => {
+const FirstName = ({ name, data, startDate }: FirstNameProps) => {
   return (
     <div style={{ marginBlock: "1rem" }}>
       <Typography variant="h3" component="h3" align="left">
         {name}
       </Typography>
-      <Table data={data} />
+      <Table data={data} startDate={startDate} />
     </div>
   );
 };
